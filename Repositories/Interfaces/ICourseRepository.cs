f230228e924774a4658c2f779376c7cc73749a5b
IamAbdelrahman <abdelruhman.m.kamal@gmail.com>
2025-06-28 21:08:44 +0300

    Implement Course Repository Pattern

--- Repositories/Interfaces/ICourseRepository.cs
+++ Repositories/Interfaces/ICourseRepository.cs
@@ -1,6 +1,17 @@
-﻿namespace School_Management_System.Repositories.Interfaces
+﻿using School_Management_System.Models;
+
+namespace School_Management_System.Repositories.Interfaces
 {
-    public interface ICourseRepository
+    public interface ICourseRepository:IRepository<Course>
     {
+
+        // Read Operations
+        public IEnumerable<Course> GetCoursesByName(string name);
+        public IEnumerable<Course> GetCoursesByTeacherId(int teacherId);
+        public IEnumerable<Course> GetCoursesByStudentId(int studentId);
+        public IEnumerable<Course> GetCoursesByClassId(int classId);
+        public IEnumerable<Course> GetCoursesBySubjectId(int subjectId);
+        public IEnumerable<Course> GetCoursesByDepartmentId(int departmentId);
+
     }
 }
