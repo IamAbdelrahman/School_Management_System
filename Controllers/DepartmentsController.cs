f97dd0a5e442aee0c42eda863145e2bbef24240d
sara-dissouki <sarakhalid99.sk@gmail.com>
2025-07-02 15:19:47 +0300

    Sara Updates

--- Controllers/DepartmentsController.cs
+++ Controllers/DepartmentsController.cs
@@ -1,12 +1,37 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using School_Management_System.Repositories.Implementations;
+using School_Management_System.Repositories.Interfaces;
+using School_Management_System.ViewModel;
 
 namespace School_Management_System.Controllers
 {
     public class DepartmentsController : Controller
     {
+        private readonly IDepartmentRepository departmentRepo;
+        private readonly ICourseRepository courseRepo;
+
+        public DepartmentsController(IDepartmentRepository departmentRepo, ICourseRepository courseRepo)
+        {
+            this.departmentRepo = departmentRepo;
+            this.courseRepo = courseRepo;
+        }
+
         public IActionResult Index()
         {
-            return View();
+            var courses = courseRepo.GetAll();
+            var departments = departmentRepo.GetAll();
+            var departmentVM = departments.Select(d => new DepartmentViewModel
+            {
+                DepartmentID = d.DepartmentID,
+                Name = d.Name,
+                //CoursesNames = string.Join(", ", d.Courses.Select(c => c.Name)), // ["Math", "Science"] >> "Math, Science"
+                CoursesNames = string.Join(", ", d.Courses.Select(c => c.Name)),
+                TeacherName = d.Teachers.FirstOrDefault()?.Name, // Assuming each department has at least one teacher
+                StudensCount = 0
+
+            }).ToList();
+
+            return View(departmentVM);
         }
     }
 }
---
+++ Repositories/Implementations/DepartmentRepository.cs
@@ -0,0 +1,49 @@
+﻿using School_Management_System.Models;
+using School_Management_System.Repositories.Interfaces;
+
+namespace School_Management_System.Repositories.Implementations
+{
+    public class DepartmentRepository : IDepartmentRepository
+    {
+        private ITIContext db { get; set; }
+        public DepartmentRepository(ITIContext dbcontext)
+        {
+            db = dbcontext;
+        }
+
+        public IEnumerable<Department> GetDepartmentsByName(string name)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Department> GetAll()
+        {
+            return db.Departments.ToList();
+        }
+
+        public Department GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Add(Department entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(Department entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SaveChanges()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
