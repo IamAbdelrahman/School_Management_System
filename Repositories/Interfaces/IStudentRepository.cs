00d3c194085d9eda4c5247045f5e34efa47563f0
Hagar-yusuf306 <hagaryusuf1@gmail.com>
2025-07-03 20:00:02 +0300

    first commit

--- Repositories/Interfaces/IStudentRepository.cs
+++ Repositories/Interfaces/IStudentRepository.cs
@@ -1,6 +1,10 @@
-﻿namespace School_Management_System.Repositories.Interfaces
+﻿using School_Management_System.Models;
+
+namespace School_Management_System.Repositories.Interfaces
 {
-    public interface IStudentRepository
+    public interface IStudentRepository : IRepository<Student>
     {
+        IEnumerable<Student> GetPagedAndFiltered(string? searchTerm, int pageNumber, int pageSize, out int totalCount);
+
     }
 }
