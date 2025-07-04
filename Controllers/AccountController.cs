07302ce16afa1112013a65d8a4453da0d324d6ce
IamAbdelrahman <abdelruhman.m.kamal@gmail.com>
2025-07-05 02:10:48 +0300

    Ak updates

--- Controllers/AccountController.cs
+++ Controllers/AccountController.cs
@@ -1,70 +1,70 @@
-﻿using Fluent.Infrastructure.FluentModel;
-using Microsoft.AspNetCore.Identity;
-using Microsoft.AspNetCore.Mvc;
-using School_Management_System.Models;
+﻿//using Fluent.Infrastructure.FluentModel;
+//using Microsoft.AspNetCore.Identity;
+//using Microsoft.AspNetCore.Mvc;
+//using School_Management_System.Models;
 
-namespace School_Management_System.Controllers
-{
-    public class AccountController : Controller
-    {
-        [HttpPost]
-        public async Task<IActionResult> Register(RegisterViewModel model)
-        {
-            if (ModelState.IsValid)
-            {
-                var user = new ApplicationUser
-                {
-                    UserName = model.Email,
-                    Email = model.Email
-                };
+//namespace School_Management_System.Controllers
+//{
+//    public class AccountController : Controller
+//    {
+//        [HttpPost]
+//        public async Task<IActionResult> Register(RegisterViewModel model)
+//        {
+//            if (ModelState.IsValid)
+//            {
+//                var user = new ApplicationUser
+//                {
+//                    UserName = model.Email,
+//                    Email = model.Email
+//                };
 
-                var result = await _userManager.CreateAsync(user, model.Password);
+//                var result = await _userManager.CreateAsync(user, model.Password);
 
-                if (result.Succeeded)
-                {
-                    // 👇 هنا نضيفه في Role
-                    await _userManager.AddToRoleAsync(user, "Student");
+//                if (result.Succeeded)
+//                {
+//                    // 👇 هنا نضيفه في Role
+//                    await _userManager.AddToRoleAsync(user, "Student");
 
-                    // 👇 نسجل دخوله على طول بعد التسجيل
-                    await _signInManager.SignInAsync(user, isPersistent: false);
+//                    // 👇 نسجل دخوله على طول بعد التسجيل
+//                    await _signInManager.SignInAsync(user, isPersistent: false);
 
-                    return RedirectToAction("Index", "Student"); // يروح على Dashboard
-                }
+//                    return RedirectToAction("Index", "Student"); // يروح على Dashboard
+//                }
 
-                foreach (var error in result.Errors)
-                {
-                    ModelState.AddModelError(string.Empty, error.Description);
-                }
-            }
+//                foreach (var error in result.Errors)
+//                {
+//                    ModelState.AddModelError(string.Empty, error.Description);
+//                }
+//            }
 
-            return View(model);
-        }
+//            return View(model);
+//        }
 
-        public async Task<IActionResult> Login(LoginViewModel model)
-        {
-            var result = await _signInManager.PasswordSignInAsync(model.Email, model.Password, false, false);
+//        public async Task<IActionResult> Login(LoginViewModel model)
+//        {
+//            var result = await _signInManager.PasswordSignInAsync(model.Email, model.Password, false, false);
 
-            if (result.Succeeded)
-            {
-                var user = await _userManager.FindByEmailAsync(model.Email);
-                var roles = await _userManager.GetRolesAsync(user);
+//            if (result.Succeeded)
+//            {
+//                var user = await _userManager.FindByEmailAsync(model.Email);
+//                var roles = await _userManager.GetRolesAsync(user);
 
-                if (roles.Contains("Admin"))
-                    return RedirectToAction("Index", "Admin");
+//                if (roles.Contains("Admin"))
+//                    return RedirectToAction("Index", "Admin");
 
-                if (roles.Contains("Teacher"))
-                    return RedirectToAction("Index", "Teacher");
+//                if (roles.Contains("Teacher"))
+//                    return RedirectToAction("Index", "Teacher");
 
-                if (roles.Contains("Student"))
-                    return RedirectToAction("Index", "Student");
+//                if (roles.Contains("Student"))
+//                    return RedirectToAction("Index", "Student");
 
-                return RedirectToAction("Index", "Home");
-            }
+//                return RedirectToAction("Index", "Home");
+//            }
 
-            ModelState.AddModelError("", "Invalid login attempt.");
-            return View(model);
-        }
+//            ModelState.AddModelError("", "Invalid login attempt.");
+//            return View(model);
+//        }
 
 
-    }
-}
+//    }
+//}
