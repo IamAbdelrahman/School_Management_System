49396963921132a75d9633e5ab72ccbdb22c53f7
IamAbdelrahman <abdelruhman.m.kamal@gmail.com>
2025-07-05 15:55:45 +0300

    Department Module

--- ViewModel/DepartmentViewModel.cs
+++ ViewModel/DepartmentViewModel.cs
@@ -1,11 +1,20 @@
-﻿namespace School_Management_System.ViewModel
+﻿using School_Management_System.Models;
+
+namespace School_Management_System.ViewModel
 {
+
     public class DepartmentViewModel
     {
         public int DepartmentID { get; set; }
         public string Name { get; set; }
-        public string Courses{ get; set; } 
 
-        public string? Teachers { get; set; } 
+        public string Courses { get; set; }
+        public string? Teachers { get; set; }
+
+        public List<int> SelectedCourseIDs { get; set; } = new();
+        public List<int> SelectedTeacherIDs { get; set; } = new();
+
+        public IEnumerable<Course> AllCourses { get; set; }
+        public IEnumerable<Teacher> AllTeachers { get; set; }
     }
 }
